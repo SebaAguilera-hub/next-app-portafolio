0bc6355668996ce2d405d456f0bf9bd5a989486c
Sebastian <inforaguilera@gmail.com>
2024-06-05 15:02:51 -0400

    enlace linkedin

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
     <footer className="bg-gray-800 text-white py-4">
       <div className="container mx-auto px-4 text-center">
         <div className="flex flex-col md:flex-row justify-center items-center space-y-4 md:space-y-0 md:space-x-6">
-          <Link href="https://www.linkedin.com/in/tu-perfil-linkedin" target="_blank" rel="noopener noreferrer">
+          <Link href="https://www.linkedin.com/in/sebasti%C3%A1n-aguilera-037b1a207/" target="_blank" rel="noopener noreferrer">
             <FaLinkedin className="text-gray-400 hover:text-white" size={24} />
           </Link>
           <div className="flex items-center space-x-2">
